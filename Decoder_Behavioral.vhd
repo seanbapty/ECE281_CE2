8d76e589d59d5a2d5edab38b58119ac4880bef3a
Sean Bapty <C16Sean.Bapty@usafa.edu>
2014-02-04 11:35:40 -0700

    added behavioral files

---
+++ Decoder_Behavioral.vhd
@@ -0,0 +1,56 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    21:53:45 02/03/2014 
+-- Design Name: 
+-- Module Name:    Decoder_Behavioral - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity Decoder_Behavioral is
+    Port ( I0 : in  STD_LOGIC;
+           I1 : in  STD_LOGIC;
+           EN : in  STD_LOGIC;
+           Y0 : out  STD_LOGIC;
+           Y1 : out  STD_LOGIC;
+           Y2 : out  STD_LOGIC;
+           Y3 : out  STD_LOGIC);
+end Decoder_Behavioral;
+
+architecture Behavioral of Decoder_Behavioral is
+
+	signal I0_NOT, I1_NOT : STD_LOGIC;
+
+begin
+
+I0_NOT <= Not I0;
+I1_NOT <= Not I1;
+Y0 <= I0_NOT and I1_NOT and EN;
+Y1 <= I0 and I1_NOT and EN;
+Y2 <= I0_NOT and I1 and EN;
+Y3 <= I0 and I1 and EN;
+
+end Behavioral;
+
---
+++ Decoder_Behavioral_testbench.vhd
@@ -0,0 +1,128 @@
+--------------------------------------------------------------------------------
+-- Company: 
+-- Engineer:
+--
+-- Create Date:   22:38:01 02/03/2014
+-- Design Name:   
+-- Module Name:   C:/Users/C16Sean.Bapty/Documents/Academics 1.0/Spring 2014/ECE 281/CE2/Decoder_Behavioral_testbench.vhd
+-- Project Name:  CE2
+-- Target Device:  
+-- Tool versions:  
+-- Description:   
+-- 
+-- VHDL Test Bench Created by ISE for module: Decoder_Behavioral
+-- 
+-- Dependencies:
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+--
+-- Notes: 
+-- This testbench has been automatically generated using types std_logic and
+-- std_logic_vector for the ports of the unit under test.  Xilinx recommends
+-- that these types always be used for the top-level I/O of a design in order
+-- to guarantee that the testbench will bind correctly to the post-implementation 
+-- simulation model.
+--------------------------------------------------------------------------------
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+ 
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--USE ieee.numeric_std.ALL;
+ 
+ENTITY Decoder_Behavioral_testbench IS
+END Decoder_Behavioral_testbench;
+ 
+ARCHITECTURE behavior OF Decoder_Behavioral_testbench IS 
+ 
+    -- Component Declaration for the Unit Under Test (UUT)
+ 
+    COMPONENT Decoder_Behavioral
+    PORT(
+         I0 : IN  std_logic;
+         I1 : IN  std_logic;
+         EN : IN  std_logic;
+         Y0 : OUT  std_logic;
+         Y1 : OUT  std_logic;
+         Y2 : OUT  std_logic;
+         Y3 : OUT  std_logic
+        );
+    END COMPONENT;
+    
+
+   --Inputs
+   signal I0 : std_logic := '0';
+   signal I1 : std_logic := '0';
+   signal EN : std_logic := '0';
+
+ 	--Outputs
+   signal Y0 : std_logic;
+   signal Y1 : std_logic;
+   signal Y2 : std_logic;
+   signal Y3 : std_logic;
+   -- No clocks detected in port list. Replace <clock> below with 
+   -- appropriate port name 
+ 
+ 
+ 
+BEGIN
+ 
+	-- Instantiate the Unit Under Test (UUT)
+   uut: Decoder_Behavioral PORT MAP (
+          I0 => I0,
+          I1 => I1,
+          EN => EN,
+          Y0 => Y0,
+          Y1 => Y1,
+          Y2 => Y2,
+          Y3 => Y3
+        );
+
+  
+
+   -- Stimulus process
+   stim_proc: process
+   begin		
+     	I0 <= '0';
+		I1 <= '0';
+		EN <= '0';
+		--assert F='1' report "000 Fail" severity note;
+		wait for 100 ns;	
+		
+		-- Second line of truth table
+		I0 <= '0';
+		I1 <= '0';
+		EN <= '1';
+		wait for 100 ns;	
+		
+		-- Complete_ the rest of the truth table below:
+		I0 <= '0';
+		I1 <= '1';
+		EN <= '0';
+		wait for 100 ns;	
+		I0 <= '0';
+		I1 <= '1';
+		EN <= '1';
+		wait for 100 ns;	
+		I0 <= '1';
+		I1 <= '0';
+		EN <= '0';
+		wait for 100 ns;	
+		I0 <= '1';
+		I1 <= '0';
+		EN <= '1';
+		wait for 100 ns;	
+		I0 <= '1';
+		I1 <= '1';
+		EN <= '0';
+		wait for 100 ns;	
+		I0 <= '1';
+		I1 <= '1';
+		EN <= '1';
+		wait for 100 ns;	
+      wait;
+   end process;
+
+END;
